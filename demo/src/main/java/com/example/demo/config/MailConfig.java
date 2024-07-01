687371bb6fb55eb69bbe049df770332d4ec11e67
홍종훈 <whdgnszz1@snu.ac.kr>
2024-07-01 17:26:01 +0900

    Setting: 테스트를 위한 토이프로젝트 세팅

---
+++ demo/src/main/java/com/example/demo/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // SMTP 서버 주소
+        mailSender.setPort(587); // SMTP 서버 포트
+
+        mailSender.setUsername("your-email@gmail.com"); // 발신자 이메일 주소
+        mailSender.setPassword("your-email-password"); // 발신자 이메일 패스워드
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
